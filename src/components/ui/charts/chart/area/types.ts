504c90f55d8b4fe4582d0a960aad8c0f63f45167
Jason <jason@hedmans.org>
2025-09-28 13:43:54 -0400

    add deploy action

--- src/components/ui/charts/chart/area/types.ts
+++ src/components/ui/charts/chart/area/types.ts
@@ -1,7 +1,7 @@
 import type { AreaProps } from "recharts";
 import type { ChartProps, Series } from "../types";
 
-export type Area<T extends Record<string, number>> = Series<T, AreaProps>;
+type Area<T extends Record<string, number>> = Series<T, AreaProps>;
 
 export interface AreaChartProps<T extends Record<string, number>>
   extends ChartProps<T> {
--- src/components/ui/charts/chart/bar/types.ts
+++ src/components/ui/charts/chart/bar/types.ts
@@ -1,7 +1,7 @@
 import type { BarProps } from "recharts";
 import type { ChartProps, Series } from "../types";
 
-export type Bar<T extends Record<string, number>> = Series<T, BarProps>;
+type Bar<T extends Record<string, number>> = Series<T, BarProps>;
 
 export interface BarChartProps<T extends Record<string, number>>
   extends ChartProps<T> {
