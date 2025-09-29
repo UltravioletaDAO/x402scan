64915019dede6d4800097fe16a8abda12b3edd3d
Jason <jason@hedmans.org>
2025-09-29 19:47:32 -0400

    lint and knip

--- src/app/(home)/(overview)/_components/latest-transactions/index.tsx
+++ src/app/(home)/(overview)/_components/latest-transactions/index.tsx
@@ -15,21 +15,37 @@ export const LatestTransactions = () => {
 
   return (
     <HydrateClient>
-      <div className="flex flex-col gap-4">
-        <div>
-          <h2 className="text-2xl font-bold">Latest Transactions</h2>
-          <p className="text-muted-foreground">
-            Latest x402 transactions made using the Coinbase faciltiator
-          </p>
-        </div>
+      <LatestTransactionsTableContainer>
         <Suspense fallback={<LoadingLatestTransactionsTable />}>
           <LatestTransactionsTable limit={limit} />
         </Suspense>
-      </div>
+      </LatestTransactionsTableContainer>
     </HydrateClient>
   );
 };
 
 export const LoadingLatestTransactions = () => {
-  return <LoadingLatestTransactionsTable />;
+  return (
+    <LatestTransactionsTableContainer>
+      <LoadingLatestTransactionsTable />
+    </LatestTransactionsTableContainer>
+  );
+};
+
+const LatestTransactionsTableContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <h2 className="text-2xl font-bold">Latest Transactions</h2>
+        <p className="text-muted-foreground">
+          Latest x402 transactions made using the Coinbase faciltiator
+        </p>
+      </div>
+      {children}
+    </div>
+  );
 };
