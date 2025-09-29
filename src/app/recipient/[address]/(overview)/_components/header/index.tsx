aa47a582d3586870325c5416828c219038680318
Jason <jason@hedmans.org>
2025-09-29 15:53:09 -0400

    add buttons to recipient header

--- src/app/recipient/[address]/(overview)/_components/header/index.tsx
+++ src/app/recipient/[address]/(overview)/_components/header/index.tsx
@@ -13,6 +13,7 @@ import { cn, formatAddress } from '@/lib/utils';
 
 import { api } from '@/trpc/server';
 import { Address } from '@/components/address';
+import { HeaderButtons, LoadingHeaderButtons } from './buttons';
 
 interface Props {
   address: string;
@@ -65,9 +66,7 @@ export const HeaderCard: React.FC<Props> = async ({ address }) => {
               )}
             </p>
           </div>
-          {/* <Suspense fallback={<LoadingHeaderButtons />}>
-            <HeaderButtons address={address} />
-          </Suspense> */}
+          <HeaderButtons hasOrigins={origins.length > 0} />
         </div>
         <div className="col-span-2">
           <Suspense fallback={<LoadingOverallRecipientStats />}>
@@ -82,20 +81,21 @@ export const HeaderCard: React.FC<Props> = async ({ address }) => {
 export const LoadingHeaderCard = () => {
   return (
     <Card className={cn('relative mt-10 md:mt-12 mb-12')}>
-      <div className="absolute top-0 left-4 -translate-y-1/2 size-12 md:size-16 flex items-center justify-center">
+      <Card className="absolute top-0 left-4 -translate-y-1/2 size-12 md:size-16 flex items-center justify-center border rounded-md overflow-hidden">
         <Avatar
           src={undefined}
           className="size-full"
           fallback={<Skeleton className="size-8" />}
         />
-      </div>
+      </Card>
 
       <div className="grid grid-cols-1 md:grid-cols-7">
-        <div className="flex flex-col gap-4 p-4 pt-12 md:pt-14 col-span-5">
+        <div className="flex flex-col gap-4 p-4 pt-8 md:pt-10 col-span-5">
           <div className="">
             <Skeleton className="w-36 h-[30px] my-[3px]" />
             <Skeleton className="w-64 h-[16px] my-[4px]" />
           </div>
+          <LoadingHeaderButtons />
         </div>
         <div className="col-span-2 overflow-hidden">
           <LoadingOverallRecipientStats />
