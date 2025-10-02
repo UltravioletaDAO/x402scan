3f409d20cc0a8d9b820abb4b5e7a176c830e49ac
Jason <jason@hedmans.org>
2025-10-02 15:28:24 -0400

    update facilitator links

--- src/lib/facilitators.ts
+++ src/lib/facilitators.ts
@@ -24,7 +24,7 @@ const coinbaseFacilitator: Facilitator = {
   id: 'coinbase',
   name: 'Coinbase',
   image: '/coinbase.png',
-  link: 'https://coinbase.com',
+  link: 'https://docs.cdp.coinbase.com/x402/welcome',
   addresses: ['0xdbdf3d8ed80f84c35d01c6c9f9271761bad90ba6'],
 };
 
@@ -32,7 +32,7 @@ const x402rsFacilitator: Facilitator = {
   id: 'x402rs',
   name: 'X402rs',
   image: '/x402rs.png',
-  link: 'https://x402rs.com',
+  link: 'https://x402.rs',
   addresses: ['0xd8dfc729cbd05381647eb5540d756f4f8ad63eec'],
 };
 
@@ -40,7 +40,7 @@ const payAiFacilitator: Facilitator = {
   id: 'payai',
   name: 'PayAI',
   image: '/payai.png',
-  link: 'https://payai.com',
+  link: 'https://payai.network',
   addresses: ['0xc6699d2aada6c36dfea5c248dd70f9cb0235cb63'],
 };
 
