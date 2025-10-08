e8bab0f6ece6763dcd9e1fb1e4623425f568f04b
Jason <jason@hedmans.org>
2025-10-08 11:17:40 -0400

    add posthog to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { PostHogProvider } from './_contexts/posthog';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -122,50 +123,54 @@ export default function RootLayout({
         <TRPCReactProvider>
           <SearchProvider>
             <CDPHooksProvider>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="light"
-                storageKey="x402scan-theme"
-                enableSystem={true}
-              >
-                <div className="min-h-screen flex flex-col relative">
-                  <LogoContainer>
-                    <Link href="/" prefetch={false}>
-                      <Logo className="size-full aspect-square" />
-                    </Link>
-                  </LogoContainer>
-                  <header className="w-full flex flex-col pt-4 justify-center bg-card">
-                    <div className="flex items-center justify-between w-full px-2 md:px-6 pb-0 md:pb-0 h-10">
-                      <div className="pl-8 md:pl-8 flex items-center gap-2 md:gap-3">
-                        {breadcrumbs}
+              <PostHogProvider>
+                <ThemeProvider
+                  attribute="class"
+                  defaultTheme="light"
+                  storageKey="x402scan-theme"
+                  enableSystem={true}
+                >
+                  <div className="min-h-screen flex flex-col relative">
+                    <LogoContainer>
+                      <Link href="/" prefetch={false}>
+                        <Logo className="size-full aspect-square" />
+                      </Link>
+                    </LogoContainer>
+                    <header className="w-full flex flex-col pt-4 justify-center bg-card">
+                      <div className="flex items-center justify-between w-full px-2 md:px-6 pb-0 md:pb-0 h-10">
+                        <div className="pl-8 md:pl-8 flex items-center gap-2 md:gap-3">
+                          {breadcrumbs}
+                        </div>
+                        <div className="flex items-center gap-1 md:gap-2">
+                          <NavbarSearchButton />
+                          <NavbarAuthButton />
+                          <a
+                            href="https://github.com/Merit-Systems/x402scan"
+                            target="_blank"
+                          >
+                            <Button variant="outline" size={'navbar'}>
+                              <Image
+                                src="/github.png"
+                                alt="GitHub"
+                                width={16}
+                                height={16}
+                                className="size-4"
+                              />
+                              <span className="hidden md:block">
+                                Contribute
+                              </span>
+                            </Button>
+                          </a>
+                          <AnimatedThemeToggler />
+                        </div>
                       </div>
-                      <div className="flex items-center gap-1 md:gap-2">
-                        <NavbarSearchButton />
-                        <NavbarAuthButton />
-                        <a
-                          href="https://github.com/Merit-Systems/x402scan"
-                          target="_blank"
-                        >
-                          <Button variant="outline" size={'navbar'}>
-                            <Image
-                              src="/github.png"
-                              alt="GitHub"
-                              width={16}
-                              height={16}
-                              className="size-4"
-                            />
-                            <span className="hidden md:block">Contribute</span>
-                          </Button>
-                        </a>
-                        <AnimatedThemeToggler />
-                      </div>
+                    </header>
+                    <div className="bg-background flex-1 flex flex-col">
+                      {children}
                     </div>
-                  </header>
-                  <div className="bg-background flex-1 flex flex-col">
-                    {children}
                   </div>
-                </div>
-              </ThemeProvider>
+                </ThemeProvider>
+              </PostHogProvider>
             </CDPHooksProvider>
           </SearchProvider>
         </TRPCReactProvider>
