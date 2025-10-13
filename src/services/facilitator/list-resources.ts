498469aef098797e6f84ba7abdf49a1cffa7db6f
Jason <jason@hedmans.org>
2025-10-13 13:01:43 -0400

    list all resources when over pagination limit

--- src/services/facilitator/list-resources.ts
+++ src/services/facilitator/list-resources.ts
@@ -1,10 +1,40 @@
 import 'server-only';
 
-import type { FacilitatorConfig } from 'x402/types';
+import type {
+  FacilitatorConfig,
+  ListDiscoveryResourcesRequest,
+} from 'x402/types';
 import { useFacilitator as facilitatorUtils } from 'x402/verify';
 
+type FacilitatorResource = Awaited<
+  ReturnType<typeof listFacilitatorResources>
+>['items'][number];
+
 export const listFacilitatorResources = async (
+  facilitator: FacilitatorConfig,
+  config?: ListDiscoveryResourcesRequest
+) => {
+  return await facilitatorUtils(facilitator).list(config);
+};
+
+export const listAllFacilitatorResources = async (
   facilitator: FacilitatorConfig
 ) => {
-  return await facilitatorUtils(facilitator).list();
+  let hasMore = true;
+  let offset = 0;
+  const allItems: FacilitatorResource[] = [];
+  while (hasMore) {
+    const { pagination, items } = await listFacilitatorResources(facilitator, {
+      offset,
+      limit: 100,
+    });
+    allItems.push(...items);
+    if (pagination.total > pagination.offset + pagination.limit) {
+      hasMore = true;
+      offset += pagination.limit;
+    } else {
+      hasMore = false;
+    }
+  }
+  return allItems;
 };
