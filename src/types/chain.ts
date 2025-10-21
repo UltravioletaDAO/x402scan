a36a009491ae876c04426da14d69b44fc69e95ff
shafu <shafu0x@gmail.com>
2025-10-21 13:38:04 -0400

    chain icons

--- src/types/chain.ts
+++ src/types/chain.ts
@@ -10,4 +10,9 @@ export const CHAIN_LABELS: Record<Chain, string> = {
     [Chain.SOLANA]: 'Solana',
 }
 
+export const CHAIN_ICONS: Record<Chain, string> = {
+    [Chain.BASE]: '/base.png',
+    [Chain.SOLANA]: '/solana.jpg',
+}
+
 export const DEFAULT_CHAIN = Chain.BASE;
