dc0b2f3645dd2d75d0902872089ba86f94c57431
Jason <jason@hedmans.org>
2025-10-20 21:21:23 -0400

    force chat page dynamic

--- src/app/composer/(chat)/chat/(new)/page.tsx
+++ src/app/composer/(chat)/chat/(new)/page.tsx
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { Chat } from '../_components/chat';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ChatPage() {
   const id = uuidv4();
 
