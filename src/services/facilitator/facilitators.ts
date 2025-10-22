c4574eb6c48f8bf8b4f743bdd96d0667f8ae2c74
Jason <jason@hedmans.org>
2025-10-22 10:41:09 -0400

    no special list endpoints

--- src/services/facilitator/facilitators.ts
+++ src/services/facilitator/facilitators.ts
@@ -2,26 +2,16 @@ import { facilitator as baseFacilitator } from '@coinbase/x402';
 
 import type { FacilitatorConfig } from 'x402/types';
 
-export type ExtendedFacilitatorConfig = FacilitatorConfig & {
-  useListEndpoint?: boolean;
-};
-
-const payAiFacilitator: ExtendedFacilitatorConfig = {
+const payAiFacilitator: FacilitatorConfig = {
   url: 'https://facilitator.payai.network',
 };
 
-const thirdwebFacilitator: ExtendedFacilitatorConfig = {
+const thirdwebFacilitator: FacilitatorConfig = {
   url: 'https://api.thirdweb.com/v1/payments/x402',
-};
-
-const payAiSolanaFacilitator: ExtendedFacilitatorConfig = {
-  url: 'https://facilitator.payai.network',
-  useListEndpoint: true,
 };
 
 export const facilitators: FacilitatorConfig[] = [
   baseFacilitator,
   payAiFacilitator,
   thirdwebFacilitator,
-  payAiSolanaFacilitator,
 ];
--- src/services/facilitator/list-resources.ts
+++ src/services/facilitator/list-resources.ts
@@ -3,53 +3,18 @@ import 'server-only';
 import type {
   FacilitatorConfig,
   ListDiscoveryResourcesRequest,
-  ListDiscoveryResourcesResponse,
 } from 'x402/types';
 import { useFacilitator as facilitatorUtils } from 'x402/verify';
-import type { ExtendedFacilitatorConfig } from './facilitators';
 
 type FacilitatorResource = Awaited<
   ReturnType<typeof listFacilitatorResources>
 >['items'][number];
 
 const listFacilitatorResources = async (
-  facilitator: ExtendedFacilitatorConfig,
-  config?: ListDiscoveryResourcesRequest
-) => {
-  if (facilitator.useListEndpoint) {
-    return await listResourcesFromListEndpoint(facilitator, config);
-  }
-  // NOTE(shafu): use the default discovery endpoint
-  return await facilitatorUtils(facilitator).list(config);
-};
-
-// Custom function to fetch from /list endpoint
-const listResourcesFromListEndpoint = async (
   facilitator: FacilitatorConfig,
   config?: ListDiscoveryResourcesRequest
-): Promise<ListDiscoveryResourcesResponse> => {
-  const params = new URLSearchParams(
-    Object.fromEntries(
-      Object.entries(config ?? {}).map(([key, value]) => [
-        key,
-        value?.toString() ?? '',
-      ])
-    )
-  ).toString();
-  const url = `${facilitator.url}/list`;
-  const res = await fetch(`${url}?${params}`, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (res.status !== 200) {
-    throw new Error(
-      `Failed to list resources: ${res.status} ${res.statusText}`
-    );
-  }
-
-  const data = (await res.json()) as ListDiscoveryResourcesResponse;
-  return data;
+) => {
+  return await facilitatorUtils(facilitator).list(config);
 };
 
 export const listAllFacilitatorResources = async (
