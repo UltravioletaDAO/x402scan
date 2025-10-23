9e4d6ca66276d3ade1527919ac896fe9aebddb7c
shafu <shafu0x@gmail.com>
2025-10-23 12:01:36 -0400

    fix chain selector on mobile

--- src/app/_components/layout/navbar/chain-selector.tsx
+++ src/app/_components/layout/navbar/chain-selector.tsx
@@ -26,7 +26,7 @@ export const ChainSelector = () => {
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger asChild>
-        <Button variant="outline" size="navbar" className="min-w-[120px]">
+        <Button variant="outline" size="navbar" className="md:min-w-[120px]">
           {chain ? (
             <Image
               src={CHAIN_ICONS[chain]}
