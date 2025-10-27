13e7444bb5f1b31052a50bf77187e1941e8f2803
Jason <jason@hedmans.org>
2025-10-27 11:38:49 -0400

    read replicas for transfers db

--- src/services/transfers/client.ts
+++ src/services/transfers/client.ts
@@ -1,30 +1,33 @@
 import { PrismaClient as TransfersPrismaClient } from '.prisma/client-transfers';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
-import ws from 'ws';
+import { readReplicas } from '@prisma/extension-read-replicas';
 import { env } from '@/env';
 import type { Sql } from '@prisma/client/runtime/library';
 import type z from 'zod';
 
-neonConfig.webSocketConstructor = ws;
-
-const connectionString = env.TRANSFERS_DB_URL;
-const adapter = new PrismaNeon({ connectionString });
-
-const globalForTransfersPrisma = global as unknown as {
-  transfersPrisma: TransfersPrismaClient;
-};
-
-export const transfersPrisma =
-  globalForTransfersPrisma.transfersPrisma ??
-  new TransfersPrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForTransfersPrisma.transfersPrisma = transfersPrisma;
-}
+export const transfersPrisma = new TransfersPrismaClient().$extends(
+  readReplicas({
+    url: [
+      ...(env.TRANSFERS_DB_URL_REPLICA_1
+        ? [env.TRANSFERS_DB_URL_REPLICA_1]
+        : []),
+      ...(env.TRANSFERS_DB_URL_REPLICA_2
+        ? [env.TRANSFERS_DB_URL_REPLICA_2]
+        : []),
+      ...(env.TRANSFERS_DB_URL_REPLICA_3
+        ? [env.TRANSFERS_DB_URL_REPLICA_3]
+        : []),
+      ...(env.TRANSFERS_DB_URL_REPLICA_4
+        ? [env.TRANSFERS_DB_URL_REPLICA_4]
+        : []),
+      ...(env.TRANSFERS_DB_URL_REPLICA_5
+        ? [env.TRANSFERS_DB_URL_REPLICA_5]
+        : []),
+    ],
+  })
+);
 
 export const queryRaw = async <T>(sql: Sql, resultSchema: z.ZodSchema<T>) => {
-  const result = await transfersPrisma.$queryRaw<T>(sql);
+  const result = await transfersPrisma.$replica().$queryRaw<T>(sql);
   const parseResult = resultSchema.safeParse(result);
   if (!parseResult.success) {
     throw new Error('Invalid result: ' + parseResult.error.message);
